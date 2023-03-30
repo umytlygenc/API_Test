1f2f4937d5c8a0c34a3a6e6dce68306769e9ec0b
umytlygenc <hopetrade290@gmail.com>
2023-03-31 01:51:16 +0300

    api

---
+++ src/test/java/test_data/JsonPlaceHolderTestData.java
@@ -0,0 +1,30 @@
+package test_data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class JsonPlaceHolderTestData {
+
+    public Map<String, Object> expectedDataMethod(Integer userId, String title, Boolean completed) {
+
+        Map<String, Object> expectedData = new HashMap<>();
+        if (userId != null) {//Eklemek istemediğim değerleri null atıyorum
+            expectedData.put("userId", userId);
+        }
+        if (title != null) {
+            expectedData.put("title", title);
+        }
+
+        if (completed != null) {
+            expectedData.put("completed", completed);
+        }
+
+        return expectedData;
+    }
+
+    public static String expectedDataInString(Integer userId, String title, Boolean completed){
+
+        return "{ \"userId\": "+userId+", \"title\": \""+title+"\", \"completed\": "+completed+" }";
+
+    }
+}
